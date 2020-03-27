08e3ed9b0cf39206f84ff0fb755afbbffcb74bdd
Elias Taveras <eliastave12@gmail.com>
2020-03-27 03:26:40 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -14,7 +14,7 @@ def consolidate_cart(cart)
   cart_list = []
   counter = 0
     while counter < cart.length do
-      
+      cart
   cart.each { |grocery| grocery.each { |product,values| consolidated_cart[product] = {price: values[:price], clearance: values[:clearance], count: cart.count(grocery)} } }
   consolidated_cart
 end
