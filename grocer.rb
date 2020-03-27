2c6daebb590667431b66a47658e56c2ebcad5b28
Elias Taveras <eliastave12@gmail.com>
2020-03-27 03:48:38 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -40,6 +40,7 @@ def apply_coupons(cart, coupons)
     cart_item_with_coupon = find_item_by_name_in_collection(couponed_item_name, cart)
       if cart_item && cart_item[:count] >= coupons[i][:num]
       if cart_item_with_coupon
+        cart_item_with_coupon[:count] += coupons[i][:num]
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
