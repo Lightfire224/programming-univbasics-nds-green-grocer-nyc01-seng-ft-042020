3b7e73f3d3595252d43a9550170af8f8a9d9e686
Elias Taveras <eliastave12@gmail.com>
2020-03-27 03:49:44 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -60,9 +60,14 @@ end
 
 
 def apply_clearance(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  i=0
+  while i < cart.length
+    if cart[i][:clearance]==true
+       cart[i][:price]=(cart[i][:price]*0.8).round(2)
+    end
+    i+=1
+  end
+  cart
 end
 
 def checkout(cart, coupons)
